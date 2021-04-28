03d530776c92b52237e6d0b3c539f6e7b451868f
Toby <tobyxdd@gmail.com>
2021-04-27 20:21:01 -0700

    Fix stubs

--- pkg/tproxy/udp_stub.go
+++ pkg/tproxy/udp_stub.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"github.com/tobyxdd/hysteria/pkg/acl"
 	"github.com/tobyxdd/hysteria/pkg/core"
+	"github.com/tobyxdd/hysteria/pkg/transport"
 	"net"
 	"time"
 )
@@ -14,7 +15,8 @@ var ErrTimeout = errors.New("inactivity timeout")
 
 type UDPTProxy struct{}
 
-func NewUDPTProxy(hyClient *core.Client, listen string, timeout time.Duration, aclEngine *acl.Engine,
+func NewUDPTProxy(hyClient *core.Client, transport transport.Transport, listen string, timeout time.Duration,
+	aclEngine *acl.Engine,
 	connFunc func(addr net.Addr), errorFunc func(addr net.Addr, err error)) (*UDPTProxy, error) {
 	return nil, errors.New("not supported on the current system")
 }
