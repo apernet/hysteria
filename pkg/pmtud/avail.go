9eb0ea4367f3aa9d78f1d507f976e5ced5d71fcf
Toby <tobyxdd@gmail.com>
2022-11-23 00:15:17 -0800

    chore: add explaination comment to pmtud flag

--- pkg/pmtud/avail.go
+++ pkg/pmtud/avail.go
@@ -3,6 +3,12 @@
 
 package pmtud
 
+// quic-go's MTU discovery is by default enabled on all platforms. However, we found that it
+// does not set DF bit correctly on some platforms (macOS for example), which causes the probe
+// packets (which should never be fragmented) to be fragmented and sent anyway. So here in
+// Hysteria we only enable it on Linux and Windows for now, where we have tested it and can
+// confirm that it works correctly.
+
 const (
 	DisablePathMTUDiscovery = false
 )
