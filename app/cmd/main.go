aa7766b47e2877dfd1c440d2083340031bed2b5b
KsaNL <73447685+KsaNL@users.noreply.github.com>
2023-05-17 18:41:47 +0800

    Add Win32 VT mode support

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"os"
 	"regexp"
+	"runtime"
 	"strings"
 	"time"
 
@@ -14,6 +15,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"golang.org/x/sys/windows"
 )
 
 const (
@@ -142,6 +144,21 @@ var serverCmd = &cobra.Command{
 	},
 }
 
+// Add console VT color mode
+func openWinVT() {
+	if runtime.GOOS != "windows" {
+		return
+	}
+	stdout := windows.Handle(os.Stdout.Fd())
+
+	var mode uint32
+	windows.GetConsoleMode(stdout, &mode)
+
+	mode |= windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING // Add VT Color Support
+
+	windows.SetConsoleMode(stdout, mode)
+}
+
 // fakeFlags replace the old flag format with the new format(eg: `-config` ->> `--config`)
 func fakeFlags() {
 	var args []string
@@ -157,6 +174,9 @@ func fakeFlags() {
 }
 
 func init() {
+	//
+	openWinVT()
+	
 	// compatible with old flag format
 	fakeFlags()
 
