apernet/hysteria

commit 3eeab8a34923ce635bb620cd7e6760d8a9178fa9

fix: use homedir as workdir in systemd unit
as default acme storage is $workdir/acme instead of $homedir/.config/...

scripts/install_server.sh
@@ -646,7 +646,7 @@ After=network.target
 [Service]
 Type=simple
 ExecStart=$EXECUTABLE_INSTALL_PATH server --config ${_config_name}.yaml
-WorkingDirectory=$CONFIG_DIR
+WorkingDirectory=$HYSTERIA_HOME_DIR
 User=$HYSTERIA_USER
 Group=$HYSTERIA_USER
 Environment=HYSTERIA_LOG_LEVEL=info

commit ec91aa592ac52ee7551e16aa906318798ff6e56c

fix: confdir in systemd unit in install_server.sh
caused by 3eeab8a34923ce635bb620cd7e6760d8a9178fa9

scripts/install_server.sh
@@ -649,7 +649,7 @@ After=network.target
 
 [Service]
 Type=simple
-ExecStart=$EXECUTABLE_INSTALL_PATH server --config ${_config_name}.yaml
+ExecStart=$EXECUTABLE_INSTALL_PATH server --config ${CONFIG_DIR}/${_config_name}.yaml
 WorkingDirectory=$HYSTERIA_HOME_DIR
 User=$HYSTERIA_USER
 Group=$HYSTERIA_USER